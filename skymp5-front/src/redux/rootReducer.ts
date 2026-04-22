3ad6df43557645214fae99346da27d07264fe3fa
Piotr Roszkowski <piotroszko2@gmail.com>
2026-04-22 21:11:02 +0200

    refactor: added recommended redux toolkit, refactor to ts

--- skymp5-front/src/redux/rootReducer.ts
+++ skymp5-front/src/redux/rootReducer.ts
@@ -1,4 +1,4 @@
-import { combineReducers } from "redux";
+import { combineReducers } from "@reduxjs/toolkit";
 
 import { appReducer } from "../reducers/app";
 import { commandReducer } from "../reducers/command";
